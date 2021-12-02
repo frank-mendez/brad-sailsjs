eb34b564b1612de74305ebee9b5ef31a1a952080
frankmendez <frankmendezresources@gmail.com>
2021-12-03 00:41:41 +0800

    finished project

--- api/controllers/ArticlesController.js
+++ api/controllers/ArticlesController.js
@@ -6,7 +6,54 @@
  */
 
 module.exports = {
-  
-
+  list: async (req, res) => {
+    try {
+      let articles = await Articles.find({});
+      res.view("pages/list", { articles });
+    } catch (error) {
+      res.serverError(error);
+    }
+  },
+  add: (req, res) => {
+    res.view("pages/add");
+  },
+  create: async (req, res) => {
+    const title = req.body.title;
+    const body = req.body.body;
+    try {
+      await Articles.create({ title, body }).fetch();
+      res.redirect("/articles/list");
+    } catch (error) {
+      res.send(500, { error: "Database error" });
+    }
+  },
+  delete: async (req, res) => {
+    const id = req.param("id");
+    try {
+      await Articles.destroyOne({ id });
+      res.redirect("/articles/list");
+    } catch (error) {
+      res.send(500, { error });
+    }
+  },
+  edit: async (req, res) => {
+    const id = req.param("id");
+    try {
+      const article = await Articles.findOne({ id });
+      res.view("pages/edit", { article });
+    } catch (error) {
+      res.send(500, { error });
+    }
+  },
+  update: async (req, res) => {
+    const title = req.body.title;
+    const body = req.body.body;
+    const id = req.body.id;
+    try {
+      await Articles.updateOne({ id }).set({ title, body });
+      res.redirect("/articles/list");
+    } catch (error) {
+      res.send(500, { error });
+    }
+  },
 };
-
